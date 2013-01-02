68d31aed74b16b95d093ff36788fb599836a1df4
JeanHuit <kollecter@gmail.com>
2013-01-02 14:53:12 +0000

    file commit

---
+++ js/ours.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: jean
+ * Date: 12/24/12
+ * Time: 5:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function(){
+
+
+
+
+
+});
