f2342f798786b8b88ad9b686aa34a729086b2075
JeanHuit <kollecter@gmail.com>
2013-01-10 00:55:25 +0000

    working somewhat, remaining reaction in search bar

--- js/ours.js
+++ js/ours.js
@@ -6,7 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 $(document).ready(function(){
-
+/*
     $.getJSON('document.json', function(data) {
         var output="<p>";
         for (var i in data.hymns) {
@@ -15,7 +15,28 @@ $(document).ready(function(){
 
         output+="</p>";
         document.getElementById("content").innerHTML=output;
-    });
+    });*/
+$('#search-2').textinput({
+
+create:
+
+    $.getJSON('document.json', function(data) {
+        var output="<p>";
+
+
+        var i=Number($('#search-2').val()) + 1;
+
+            output+="<p>"+ data.hymns[i].verse +"</p>";
+
+
+        output+="</p>";
+        document.getElementById("content").innerHTML=output;
+    })
+});
+
+
+
+
 
 
 
